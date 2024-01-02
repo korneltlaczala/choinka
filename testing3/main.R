fe5b2281eacb3a9c7a039bad0ef8f73a9e059787
korneltlaczala <kornel.tlaczala@gmail.com>
2024-01-02 05:52:49 +0100

    trunk works

--- testing3/main.R
+++ testing3/main.R
@@ -1,35 +1,60 @@
 library(plotly)
 library(ggplot2)
 
-generate_tree_plot <- function(ppr = 100, revolutions = 1, branch_count = 10) {
-    arclen <- 2 * pi
+arclen <- 2 * pi
+plot <- plot_ly()
+
+generate_tree_plot <- function(ppr = 100, revolutions = 1, branch_count = 10, height = 10) {
     point_count <- ceiling(ppr * revolutions)
 
-    height <- 10
-    width <- 0.3
+    width <- height * 3/10
     branch_offset <- arclen / branch_count
     branch_offset <- seq(from = 0, by = branch_offset, length.out = branch_count)
 
-    z <- rep(seq(0, height, length.out = point_count), branch_count)
+    z <- rep(seq(0, height, length.out = point_count), 2*branch_count)
 
     spread <- (1:point_count) / ppr * arclen
     grid <- expand.grid(spread, branch_offset)
     spread <- grid$Var1 + grid$Var2
+    spread <- c(spread, rev(spread))
 
-    x <- sin(spread) * rev(z) * width
-    y <- cos(spread) * rev(z) * width
+    x <- sin(spread) * rev(z) * width / height
+    y <- cos(spread) * rev(z) * width / height
 
-    colors <- rep(c("green", "darkgreen", "forestgreen"), length.out = point_count * branch_count)
+    colors <- rep(c("green", "darkgreen", "forestgreen"), length.out = point_count * branch_count * 2)
     df <- data.frame(x, y, z, colors)
 
-    plot <- plot_ly(df,
+    plot <- plot %>% 
+        add_trace(  df,
                     x = ~x,
                     y = ~y,
                     z = ~z,
                     type = "scatter3d",
                     mode = "markers",
                     marker = list(size = 6, color = ~colors))
-    print(plot)
 }
 
-generate_tree_plot()
+generate_trunk_plot <- function(ppr = 5, height = 8.5, ring_density = 0.2, width = 0.2) {
+
+    ring_count <- floor(height / ring_density)
+    ring_heights <- rep(seq(0, height, length.out = ring_count), each = ppr)
+    spread <- rep((1:ppr) / ppr * arclen, ring_count)
+    
+    x <- sin(spread) * width
+    y <- cos(spread) * width
+
+    df <- data.frame(x, y, ring_heights)
+    
+    plot <- plot %>% 
+        add_trace(  df,
+                    x = ~x,
+                    y = ~y,
+                    z = ~ring_heights,
+                    type = "scatter3d",
+                    mode = "markers",
+                    marker = list(size = 10, color = "saddlebrown"))
+}
+
+plot <- generate_tree_plot()
+plot <- generate_trunk_plot()
+print(plot)

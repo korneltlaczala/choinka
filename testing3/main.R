f8b9cffa92d3df5a1084b79690452ad4dcad1788
korneltlaczala <kornel.tlaczala@gmail.com>
2024-01-02 03:58:57 +0100

    fix points per revolution

--- testing3/main.R
+++ testing3/main.R
@@ -1,18 +1,25 @@
 library(plotly)
 library(ggplot2)
 
-points_per_rotation <- 360*2*pi
-rotations <- 2
-point_count <- ceiling(points_per_rotation * rotations)
+ppr <- 200 # ppr - points per revolution
+revolutions <- 2
+arclen <- 2 * pi
+point_count <- ceiling(ppr * revolutions)
 height <- 10
-wide_constant <- 0.3
+width <- 0.3
 
 z <- seq(0, height, length.out = point_count)
 
-x <- sin((1:point_count)/360) * rev(z) * wide_constant
-y <- cos((1:point_count)/360) * rev(z) * wide_constant
+x <- sin((1:point_count) / ppr * arclen) * rev(z) * wide_constant
+y <- cos((1:point_count) / ppr * arclen) * rev(z) * wide_constant
 
 df <- data.frame(x, y, z)
 
-plot <- plot_ly(df, x = ~x, y = ~y, z = ~z, type = "scatter3d", mode = "markers", marker = list(size=2))
+plot <- plot_ly(df,
+                x = ~x,
+                y = ~y,
+                z = ~z,
+                type = "scatter3d",
+                mode = "markers",
+                marker = list(size = 2))
 print(plot)
